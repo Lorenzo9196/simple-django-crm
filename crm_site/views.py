ebbe68b8ee2655cb3784820b7d9d370d2c19a186
lorenzo <lorenzo9196@hotmail.com>
2023-05-30 17:01:42 -0500

    added records table - record view - delete record

--- crm_site/views.py
+++ crm_site/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    # if loged get records
+    records = Record.objects.all()
+
+    # if sending form then validate credentials
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
+            # auth and login
             login(request, user)
             messages.success(request, 'Bienvenido ' + username)
             return redirect('home')
@@ -17,7 +23,7 @@ def home(request):
             messages.error(request, 'Username or Password is incorrect')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {"records": records,})
 
 
 def logout_user(request):
@@ -32,7 +38,7 @@ def register_user(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            # auth and login
+            # register and login
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
@@ -41,5 +47,28 @@ def register_user(request):
             return redirect('home')
     else:
         form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
     return render(request,'register.html', {'form':form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+         record = Record.objects.get(id=pk)
+         return render(request, 'record.html', {"customer_record": record})
+    else:
+        messages.error(request, 'You need to login first')
+        return redirect('home')
+
+
+def delete_record(request, pk):
+
+    if request.user.is_authenticated:
+         record = Record.objects.get(id=pk)
+         record.delete()
+         records = Record.objects.all()
+
+         return render(request, 'home.html', {"records": records,})
+    else:
+        messages.error(request, 'You need to login first')
+        return redirect('home')
         
